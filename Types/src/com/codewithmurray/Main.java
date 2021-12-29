2ae023d37c22c4472654e3540260fffc7520831f
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-12-29 15:19:03 +0100

    added a string literal for using methods

--- Types/src/com/codewithmurray/Main.java
+++ Types/src/com/codewithmurray/Main.java
@@ -21,11 +21,11 @@ public class Main {
         // Type: char       Size: 2 bytes  Range: A,B,C, ...
         // boolean          Size: 1 byte   Range: true/false
 
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'M'; // Single character is single quotes
-        boolean canDrive = false;
+//        byte age = 30;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'M'; // Single character is single quotes
+//        boolean canDrive = false;
 
         // Reference Types - for storing complex objects
 
@@ -36,11 +36,20 @@ public class Main {
 
         // Primitive vs Reference Types
 
-        Point point1 = new Point(2,1);
-        Point point2 = point1;
-        point1.x = 1;
-        System.out.println(point2);
+//        Point point1 = new Point(2,1);
+//        Point point2 = point1;
+//        point1.x = 1;
+//        System.out.println(point2);
         // Note: Reference Types are copied by the references
         // Primitive Types are copied by their values and these values are independent of each other.
+
+
+        // Strings
+        String message = "Hello World" + "!!";
+        //Will return a Boolean of true
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        
+
     }
 }
